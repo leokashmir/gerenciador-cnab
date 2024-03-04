4af8ba77372e765d9ddb24aa4ba773d89f89f995
LeoKashmir <leonardobarrosbhz@gmail.com>
2024-03-04 12:34:54 -0300

    add rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PesquisaComponent } from './pesquisa/pesquisa.component';
+import { UploadfileComponent } from './uploadfile/uploadfile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'upload', component: UploadfileComponent },
+  { path: 'pesquisa', component: PesquisaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private router: Router) { };
+
+  redirectToRoute(route: string): void {
+    this.router.navigate([route]);
+  }
   title = 'Gerenciador de Arquivos CNAB';
+
+  items = [
+    { title: 'Upload de Arquivos', link: 'upload' },
+    { title: 'Consultar Transações', link: 'pesquisa' },
+  ];
 }
